035ca52079dd787fa03d37fc5877da34fa29fe35
Joe Turner <joe@oampo.co.uk>
2017-06-28 16:37:58 +0100

    Add dodgy spotify auth...

--- src/spotify.js
+++ src/spotify.js
@@ -2,13 +2,32 @@ function _search(name) {
     if (Math.random() < 0.25) {
         return Promise.reject('Something went wrong');
     }
-    return fetch(`https://api.spotify.com/v1/search?q=${name}&type=artist`)
-        .then(res => {
-            if (!res.ok) {
-                return Promise.reject(res.statusText);
+    // Don't bypass CORS like this in real life
+    return fetch('https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        body: 'grant_type=client_credentials',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            // Don't expose your credentials like this in real life
+            Authorization: `Basic ${btoa(`${process.env.REACT_APP_SPOTIFY_CLIENT_ID}:${process.env.REACT_APP_SPOTIFY_CLIENT_SECRET}`)}`
+        }
+    }).then(res => {
+        if (!res.ok) {
+            return Promise.reject(res.statusText);
+        }
+        return res.json();
+    }).then(data =>
+        fetch(`https://api.spotify.com/v1/search?q=${name}&type=artist`, {
+            headers: {
+                Authorization: `Bearer ${data.access_token}`
             }
-            return res.json();
-        }).then(data => data.artists.items.map(artist => artist.name));
+        })
+    ).then(res => {
+        if (!res.ok) {
+            return Promise.reject(res.statusText);
+        }
+        return res.json();
+    }).then(data => data.artists.items.map(artist => artist.name));
 }
 
 export function search(name) {
